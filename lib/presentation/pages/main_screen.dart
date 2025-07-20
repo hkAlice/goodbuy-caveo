985b8068bda3b32d9e5b8ed1660873aea056ab93
Alice Ogeda <alice.ogeda.s@gmail.com>
2025-07-20 17:04:57 -0300

    carregar conteudo em pages splashscreen e mainscreen;

--- lib/presentation/pages/main_screen.dart
+++ lib/presentation/pages/main_screen.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:goodbuy/shared/providers.dart';
 
 class MainScreen extends ConsumerWidget {
   const MainScreen({super.key});
 
-  // todo: construir widgets (card? listtile?)
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Placeholder();
+    final productsAsyncValue = ref.watch(productsProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GoodBuy'),
+      ),
+      body: productsAsyncValue.when(
+        data: (products) {
+          return ListView.builder(
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              final product = products.elementAt(index);
+              return Card(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 200,
+                      child: Placeholder(),
+                    ),
+                    Text(product.title),
+                    Text("testeeee oi :3")
+                  ]
+                ),
+              );
+            },
+          );
+        },
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        error: (error, stackTrace) => const Center(
+          child: Text('deu ruim'),
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/pages/splash_screen.dart
+++ lib/presentation/pages/splash_screen.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:goodbuy/presentation/pages/main_screen.dart';
+import 'package:goodbuy/shared/providers.dart';
 
 class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // todo: carregar do usecase provider e dar push screen
+    ref.listen(productsProvider, (previous, next) {
+      next.when(
+        data: (data) {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const MainScreen(),
+            ),
+          );
+        },
+        loading: () {},
+        error: (error, stackTrace) {
+          // todo: no exception, continuar para mainscreen com status de falha
+          throw Exception("nao gostou");
+        },
+      );
+    });
 
     return Material(
       child: Container(
         color: Theme.of(context).primaryColor,
         child: Center(
-            child: Text(
-          "Goodbuy",
-          style: Theme.of(context).textTheme.headlineLarge,
-        )),
+            child: Text("Goodbuy",
+            style: Theme.of(context).textTheme.headlineLarge,
+          )
+        ),
       ),
     );
   }
